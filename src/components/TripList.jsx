53a3167b0ac737718fb28da4e7deca02f3420647
akthernahela <nahela00@gmail.com>
2025-12-17 21:13:56 +0100

    Map the trips array to get the trips list

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -3,7 +3,20 @@ import { Trips } from '../assets/Trips';
 
 function TripList() {
 
-    return ()
+    return (
+        <div>
+            <h1 className="text-center mb-4">Lista dei Viaggi</h1>
+            <ul className="list-group">
+                {Trips.map(trip => (
+                    <li key={trip.id} className="list-group-item d-flex justify-content-between align-items-center">
+                        <Link to={`/trip/${trip.id}`} className="text-decoration-none">
+                            {trip.destination} – {trip.startDate} a {trip.endDate}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default TripList;
