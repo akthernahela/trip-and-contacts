b078d4fcab63fbc3b74afc6ddf793dea11ba1d9e
akthernahela <nahela00@gmail.com>
2025-12-17 22:16:33 +0100

    Add the page header bootstrap navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TripList from './components/TripList';
 import TripDetail from './components/TripDetail'
+import Header from './assets/Header';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <div className="container mt-5">
           <Routes>
             <Route path="/" element={<TripList />} />
