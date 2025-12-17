aacd49d332444d505e097b737fd4739a751fcc32
akthernahela <nahela00@gmail.com>
2025-12-17 20:59:08 +0100

    Add the two path for the two components pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TripList from './components/TripList';
+import TripDetail from './components/TripDetail'
 
 function App() {
 
 
   return (
     <>
-
+      <BrowserRouter>
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/" element={<TripList />} />
+            <Route path="/trip/:id" element={<TripDetail />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
