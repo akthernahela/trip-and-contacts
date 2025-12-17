9f8a2fc00ff903440789dad7eca7e3e59cef5b5c
akthernahela <nahela00@gmail.com>
2025-12-17 15:25:19 +0100

    first commit

---
+++ src/assets/Trips.jsx
@@ -0,0 +1,34 @@
+export const trips = [
+    {
+        id: 1,
+        destination: 'Parigi',
+        startDate: '2025-06-01',
+        endDate: '2025-06-10',
+        participants: [
+            { id: 1, name: 'Mario Rossi' },
+            { id: 2, name: 'Anna Verdi' }
+        ]
+    },
+    {
+        id: 2,
+        destination: 'Londra',
+        startDate: '2025-07-05',
+        endDate: '2025-07-12',
+        participants: [
+            { id: 3, name: 'Luigi Bianchi' },
+            { id: 5, name: 'Matteo Sini' },
+            { id: 6, name: 'Amy Santiago' }
+        ]
+    },
+    {
+        id: 3,
+        destination: 'Roma',
+        startDate: '2025-08-15',
+        endDate: '2025-08-22',
+        participants: [
+            { id: 4, name: 'Yasmin Orellana' },
+            { id: 5, name: 'Matteo Sini' },
+            { id: 6, name: 'Amy Santiago' }
+        ]
+    }
+];
