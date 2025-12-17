4273fdb41de2bc432cecb52f5ca09cd42fd5acd1
akthernahela <nahela00@gmail.com>
2025-12-17 15:33:38 +0100

    Start the list page

--- src/TripList.jsx
+++ src/TripList.jsx
@@ -1,1 +1,9 @@
 import { Trips } from '../src/assets/Trips'
+
+export default function TripList() {
+    return (
+        <>
+            <h1>Lista dei Viaggi In Programma</h1>
+        </>
+    )
+}
