34d34ca71b9f7af5b87787bba20ff16e29b5866d
akthernahela <nahela00@gmail.com>
2025-12-17 22:30:41 +0100

    I think the card is esier to connect with the participants page this way

--- src/components/TripDetail.jsx
+++ src/components/TripDetail.jsx
@@ -11,17 +11,33 @@ function TripDetail() {
 
     return (
         <>
-            <h2>{trip.destination}</h2>
-            <p>{trip.startDate} → {trip.endDate}</p>
-            <h3>Partecipanti</h3>
-            <ul>
-                {trip.participants.map(participant => (
-                    <li key={participant.id}>{participant.name}</li>
-                ))}
-            </ul>
+            <div class="card text-center">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="true" href="#">Trip</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Participants Details</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Manage</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <h2>{trip.destination}</h2>
+                    <p>{trip.startDate} → {trip.endDate}</p>
+                    <h3>Partecipanti</h3>
+                    <ul>
+                        {trip.participants.map(participant => (
+                            <li className='list-unstyled' key={participant.id}>{participant.name}</li>
+                        ))}
+                    </ul>
 
-            <Link className="btn btn-dark mt-auto" to="/">Torna alla lista</Link>
-
+                    <Link className="btn btn-dark mt-auto" to="/">Torna alla lista</Link>
+                </div>
+            </div>
         </>
     );
 }
